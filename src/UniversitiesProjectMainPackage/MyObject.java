46ca3c99c05fefc846bfd903ee3faba2e3abe3b5
kaizen-98 <ammalnassri@gmail.com>
2023-03-22 16:58:22 +0400

    frist uplode

---
+++ src/UniversitiesProjectMainPackage/MyObject.java
@@ -0,0 +1,14 @@
+package UniversitiesProjectMainPackage;
+
+import java.util.List;
+
+public class MyObject {
+
+	List<String> domains [];
+	String country;
+	List<String> web_pages [];
+	String name;
+	String alpha_two_code;
+	
+	
+}
---
+++ src/UniversitiesProjectMainPackage/TestClass.java
@@ -0,0 +1,54 @@
+package UniversitiesProjectMainPackage;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+
+public class TestClass {
+	public static void main(String[] args) {
+		String apiUrl = "http://universities.hipolabs.com/search?country=Oman";
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("HTTP error code : " + conn.getResponseCode());
+            }
+            
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+            String output;
+            StringBuilder json = new StringBuilder();
+            
+            while ((output = br.readLine()) != null) {
+                json.append(output);
+            }
+            
+            conn.disconnect();
+            
+            Gson gson = new Gson();
+            ArrayList<MyObject> myObj = gson.fromJson(json.toString(),ArrayList.class);
+            
+            for (MyObject obj : myObj) {
+                System.out.println(" "+obj.name);
+                System.out.println(" "+obj.web_pages);
+                System.out.println(" "+obj.country);
+                System.out.println(" "+obj.alpha_two_code);
+            }
+            // Use myObj for further processing
+            System.out.println("Done");
+          
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+	
+	}
+
+
