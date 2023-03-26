daf806b916aaa483cd58ec772050a3e4dc24b3f7
kaizen-98 <ammalnassri@gmail.com>
2023-03-26 12:52:55 +0400

    all working now

--- src/UniversitiesProjectMainPackage/MyObject.java
+++ src/UniversitiesProjectMainPackage/MyObject.java
@@ -4,61 +4,71 @@ import java.util.List;
 
 public class MyObject {
 
-	 private String state_province;
-	    private List<String> domains;
-	    private String country;
-	    private List<String> web_pages;
-	    private String name;
-	    private String alpha_two_code;
-	    
-		public String getState_province() {
-			return state_province;
-		}
-		
-		public MyObject(String name2, String country2, String alphaTwoCode, String stateProvince, List<String> list, List<String> list2) {
-			this.name = name2 ;
-			this.country = country2;
-			this.alpha_two_code = alphaTwoCode ; 
-			this.state_province = stateProvince;
-			this.domains = list ; 
-			this.web_pages = list2;
-			
-		}
-		public void setState_province(String state_province) {
-			this.state_province = state_province;
-		}
-		public List<String> getDomains() {
-			return domains;
-		}
-		public void setDomains(List<String> domains) {
-			this.domains = domains;
-		}
-		public String getCountry() {
-			return country;
-		}
-		public void setCountry(String country) {
-			this.country = country;
-		}
-		public List<String> getWeb_pages() {
-			return web_pages;
-		}
-		public void setWeb_pages(List<String> web_pages) {
-			this.web_pages = web_pages;
-		}
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public String getAlpha_two_code() {
-			return alpha_two_code;
-		}
-		public void setAlpha_two_code(String alpha_two_code) {
-			this.alpha_two_code = alpha_two_code;
-		}
+	private String name;
+	private String country;
+	private String stateProvince;
+	private List<String> domains;
+	private List<String> web_pages;
+	private String alpha_two_code;
 
-	    // constructors, getters and setters
+	public MyObject() {
 	}
-		
 
+	public MyObject(String name, String country, String stateProvince, List<String> domains, List<String> webPages,
+			String alphaTwoCode) {
+		this.name = name;
+		this.country = country;
+		this.stateProvince = stateProvince;
+		this.domains = domains;
+		this.web_pages = webPages;
+		this.alpha_two_code = alphaTwoCode;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getStateProvince() {
+		return stateProvince;
+	}
+
+	public void setStateProvince(String stateProvince) {
+		this.stateProvince = stateProvince;
+	}
+
+	public List<String> getDomains() {
+		return domains;
+	}
+
+	public void setDomains(List<String> domains) {
+		this.domains = domains;
+	}
+
+	public List<String> getWebPages() {
+		return web_pages;
+	}
+
+	public void setWebPages(List<String> webPages) {
+		this.web_pages = webPages;
+	}
+
+	public String getAlphaTwoCode() {
+		return alpha_two_code;
+	}
+
+	public void setAlphaTwoCode(String alphaTwoCode) {
+		this.alpha_two_code = alphaTwoCode;
+	}
+}
--- src/UniversitiesProjectMainPackage/TestClass.java
+++ src/UniversitiesProjectMainPackage/TestClass.java
@@ -1,9 +1,14 @@
 package UniversitiesProjectMainPackage;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,38 +16,58 @@ import com.google.gson.Gson;
 
 public class TestClass {
 	public static void main(String[] args) {
-		String apiUrl = "http://universities.hipolabs.com/search?country=Oman";
-        try {
-            URL url = new URL(apiUrl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
-            
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("HTTP error code : " + conn.getResponseCode());
-            }
-            
-            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-            String output;
-            StringBuilder json = new StringBuilder();
-            
-            while ((output = br.readLine()) != null) {
-                json.append(output);
-            }
-            
-            conn.disconnect();
-            
-            Gson gson = new Gson();
-            ArrayList<MyObject> myObj = gson.fromJson(json.toString(),ArrayList.class);
-            
-            // Use myObj for further processing
-            System.out.println("Done");
-          
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-	
+		String url1 = "jdbc:sqlserver://localhost:1433;" + "databaseName=javaapi;" + "encrypt=true;"
+				+ "trustServerCertificate=true";
+		String user = "sa";
+		String pass = "root";
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter country name to see university...Please Start with capital letter");
+		String country = scanner.next();
+		String apiUrl = "http://universities.hipolabs.com/search?country=" + country;
+		Connection conn1 = null;
+		try {
+			Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+			DriverManager.registerDriver(driver);
+			conn1 = DriverManager.getConnection(url1, user, pass);
+			URL url = new URL(apiUrl);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("HTTP error code : " + conn.getResponseCode());
+			}
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+			String output;
+			StringBuilder json = new StringBuilder();
+			while ((output = br.readLine()) != null) {
+				json.append(output);
+			}
+			conn.disconnect();
+			Gson gson = new Gson();
+			MyObject[] universities = gson.fromJson(json.toString(), MyObject[].class);
+			String insertSql = "INSERT INTO universities (name, domain, website, country, alpha_code) VALUES (?, ?, ?, ?, ?)";
+			PreparedStatement statement = conn1.prepareStatement(insertSql);
+			for (MyObject university : universities) {
+				System.out.println("Name: " + university.getName());
+				System.out.println("Domain: " + university.getDomains());
+				System.out.println("Website: " + university.getWebPages());
+				System.out.println("Country: " + university.getCountry());
+				System.out.println("Alpha Code: " + university.getAlphaTwoCode());
+				System.out.println();
+				statement.setString(1, university.getName());
+				statement.setString(2, university.getDomains().get(0));
+				statement.setString(3, university.getWebPages().get(0));
+				statement.setString(4, university.getCountry());
+				statement.setString(5, university.getAlphaTwoCode());
+				statement.addBatch();
+			}
+			statement.executeBatch();
+			statement.close();
+			conn1.close();// database
+			conn.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-
+}
